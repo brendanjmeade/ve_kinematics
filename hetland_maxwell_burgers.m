74c9659094256065420e56b180c3d30f362f7bd3
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-07-02 14:06:42 -0400

    More lowercase variable names

--- hetland_maxwell_burgers.m
+++ hetland_maxwell_burgers.m
@@ -1,34 +1,34 @@
 clear all;
 close all;
 
-Cycles = 40;
-Period = 10;
-delt = Period / 10;
+cycles = 40;
+period = 10;
+delt = period / 10;
 x = linspace(-10, 10, 100);
 D = 1;
-t  = [0:delt:Period*Cycles];
+t  = [0:delt:period*cycles];
 muC = 1.0;
-Tau = 0.5;
-b = struct('type', 'periodic', 'magnitude', 1.0, 'cycles', Cycles, 'period', Period, 'phase', 0.0);
+tau = 0.5;
+b = struct('type', 'periodic', 'magnitude', 1.0, 'cycles', cycles, 'period', period, 'phase', 0.0);
 
 disp(sprintf('\n\nMaxwell:'));
 Maxwell.muM = 1.0;
-Maxwell.etaM = Tau;
+Maxwell.etaM = tau;
 [phi, psi] = GIDVE_MaterialConstruct('Maxwell', 'muM', Maxwell.muM, 'etaM', Maxwell.etaM);
 [~, ~, ~, Maxwell.v] = IseisDispVE(muC, phi, psi, 40, x, t, b, D);
 
 disp(sprintf('\n\nBurgers:'));
 Burgers.muM = 1.0;
 Burgers.muV = 1.0;
-Burgers.etaM = Tau * 10;
-Burgers.etaV = Tau;
+Burgers.etaM = tau * 10;
+Burgers.etaV = tau;
 [phi, psi] = GIDVE_MaterialConstruct('Burgers', 'muM', Burgers.muM, 'muV', Burgers.muV, 'etaM', Burgers.etaM, 'etaV', Burgers.etaV);
 [~, ~, ~, Burgers.v] = IseisDispVE(muC, phi, psi, 25, x, t, b, D);
 
 % Plot the cycle invariance velocities
 figure("color", "w");
 hold on;
-tpos = (Cycles*Period-Period+1):1:Cycles*Period-1;
+tpos = (cycles*period-period+1):1:cycles*period-1;
 for i = 1:numel(tpos)
     plot(x, Maxwell.v(:, tpos(i)), 'b-');
     plot(x, Burgers.v(:, tpos(i)), 'r-');

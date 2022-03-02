bf021767e9d5e7c0bc87ca5693556ee088efc5a8
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-03-02 00:02:15 -0500

    Added velocity profile perturbation visualzation.  Need to do this for power law too.

--- ve_kinematics_basic.m
+++ ve_kinematics_basic.m
@@ -7,7 +7,7 @@ n_pts = 1000;
 x = linspace(-200e3, 200e3, n_pts);
 x_buffer_fraction = 0.1;
 y = zeros(size(x));
-t = 50 * siay;
+t = 100 * siay;
 H = 15e3;
 mu = 30e10;
 eta = 5e19;
@@ -18,13 +18,19 @@ fontsize = 18;
 n_ve_profiles = 5;
 t_vec = linspace(0, T, n_ve_profiles);
 v_viscoelastic = zeros(n_ve_profiles, n_pts);
+v_viscoelastic_perturbations = zeros(n_ve_profiles, n_pts);
+v_steady_state = 1/pi .* atan(x./H);
 for i = 1:length(t_vec)
     v_viscoelastic(i, :) = savage_2000(x, y, t_vec(i), H, mu, eta, T);
+    v_viscoelastic_perturbations(i, :) = v_viscoelastic(i, :) - v_steady_state;
 end
 v_steady_state = 1/pi .* atan(x./H);
 v_steady_state = v_steady_state(:);
 
-figure;
+
+% Plot VE profiles and perturbations from steady state
+figure("Position", [0, 0, 600, 1000]);
+subplot(2, 1, 1);
 hold on;
 ve_handle = plot(x / 1e3, v_viscoelastic, '-r', LineWidth=2.0);
 steady_state_handle = plot(x / 1e3, v_steady_state, '-b', LineWidth=2.0);
@@ -34,11 +40,31 @@ set(gca, "fontsize", fontsize);
 set(gca, "Tickdir", "out");
 set(gcf, "Color", "white")
 box on;
+set(gca, "YLim", [-2.0, 2.0]);
 set(gca, "XTick", [-200, -100, 0, 100, 200]);
 set(gca, "YTick", [-2.0, -1.0, 0.0, 1.0, 2.0]);
 set(gca, "YTickLabel", ["-2.0", "-1.0", "0.0", "1.0", "2.0"]);
 legend([ve_handle(1), steady_state_handle], "viscoelastic", "steady state")
 title("steady state and viscoelastic velocity profiles", "fontsize", fontsize, FontWeight="normal")
+
+subplot(2, 1, 2);
+hold on;
+ve_handle = plot(x / 1e3, v_viscoelastic_perturbations, '-r', LineWidth=2.0);
+steady_state_handle = plot(x / 1e3, zeros(size(v_steady_state)), '-b', LineWidth=2.0);
+xlabel("x (km)");
+ylabel("v (mm/yr)");
+set(gca, "fontsize", fontsize);
+set(gca, "Tickdir", "out");
+set(gcf, "Color", "white")
+box on;
+set(gca, "YLim", [-2.0, 2.0]);
+set(gca, "XTick", [-200, -100, 0, 100, 200]);
+set(gca, "YTick", [-2.0, -1.0, 0.0, 1.0, 2.0]);
+set(gca, "YTickLabel", ["-2.0", "-1.0", "0.0", "1.0", "2.0"]);
+legend([ve_handle(1), steady_state_handle], "viscoelastic", "steady state")
+title("viscoelastic velocity perturbations", "fontsize", fontsize, FontWeight="normal")
+
+
 
 % Define patches for basal detachment surface
 F.xmin = -220;
@@ -100,10 +126,12 @@ sgtitle("velocities from unit slip on 20 basal dislocation patches", "fontsize",
 
 
 % Solve for equivalent slip disribution
+% TODO: Show reconstruction with steady state and perturbation
 for i = 1:n_ve_profiles
-    v = v_viscoelastic(i, :);
+%     v = v_viscoelastic(i, :);
+    v = v_viscoelastic_perturbations(i, :);
     v = v(:);
-    effectiveslip = G \ v;
+%     effectiveslip = G \ v;
     if size(G, 1) > size(G, 2)
        effectiveslip = inv(G' * G) * G' * v; %#ok<*MINV> 
     elseif size(G, 2) > size(G, 1)
